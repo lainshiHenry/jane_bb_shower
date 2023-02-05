771fe1dbd207379a4eb2626ba702d6f82389839d
Henry Le <henryle913@outlook.com>
2023-02-05 00:48:03 -0600

    Changes to text

--- src/components/CannotMakeIt.js
+++ src/components/CannotMakeIt.js
@@ -7,7 +7,7 @@ const CannotMakeIt = () => {
   return (
     <div>
         <p>We're sorry you can't make it!
-            If you'd still like to contribute to a gift, please see Jane's Gift Registry
+            If you'd still like to contribute to a gift, please see Jane's Gift Registries here:
         </p>
         <p>
             <button className='CannotMakeItButton' onClick={() => {}}>
@@ -18,7 +18,7 @@ const CannotMakeIt = () => {
                 <a className='GiftRegistryLink' href='https://www.babiesrus.ca/en/babyregistry-account?registryNumber=62109015'>Babies R Us</a>
             </div>
         </p>
-        <p>If you'd like the gift delivered to the baby shower even if you can't attend. Please message Steph (204-795-6957) for arrangements.</p>
+        <p>If you'd like the gift delivered to the baby shower even if you can't attend, please message Steph (204-795-6957) for arrangements.</p>
         <p>If you have any questions, please contact: <br />Steph (204-795-6957) <br />Amy (204-294-6936)</p>
     </div>
   )
